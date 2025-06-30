8f03478e6436c7ac49ccb972f00926bcf1d14f17
Ritchie <ruw400@psu.edu>
2025-06-30 13:21:46 -0400

    support chat histories

--- infer_inst.py
+++ infer_inst.py
@@ -2,9 +2,12 @@ import torch
 import math
 import time
 import sys
+from termcolor import colored
 
 from transformers import Gemma3ForCausalLM, AutoTokenizer
 from peft import PeftModel, PeftConfig
+
+from myutils import load_chat_template
 
 assert len(sys.argv) == 2
 pretrained_model = sys.argv[1]
@@ -24,21 +27,30 @@ model = PeftModel.from_pretrained(base_model,
                                   cache_dir="/scratch/ruw400/.hf_hub", 
                                   device_map="auto").to(device)
 
+chat_template = load_chat_template("gemma_openhermes_template.txt")
+
 model.eval()
 with torch.inference_mode():
+    chats = [{"from": "system", "value": "Your name is Gemma, and you are a helpful personal assistant."}]
     while True:
         torch.cuda.empty_cache()
-        prompt = input(">>> ")
+        prompt = input(colored(">>> ", "green"))
         if prompt == "exit":
             break
-        prompt = "###user\n" + prompt + "\n\n###response\n"
-        prompt_token = torch.tensor(tokenizer.encode(prompt), dtype=torch.long).unsqueeze(0).to(device)
-        attn_mask = torch.ones_like(prompt_token, dtype=torch.long).to(device)
-        prompt_len = prompt_token.shape[1]
+        chats.append({"from": "human", "value": prompt})
+        inputs = tokenizer.apply_chat_template(chats,
+                                               tokenize=True,
+                                               chat_template=chat_template,
+                                               return_tensors="pt",
+                                               return_dict=True,
+                                               add_generation_prompt=True).to(model.device)
+        prompt_len = inputs["input_ids"].shape[1]
 
-        output_token = model.generate(prompt_token, 
-                                      attention_mask=attn_mask, 
+        output_token = model.generate(inputs["input_ids"], 
+                                      attention_mask=inputs["attention_mask"], 
                                       max_new_tokens=32_000)
         output = tokenizer.decode(output_token[0][prompt_len:], skip_special_tokens=True)
+        chats.append({"from": "gpt", "value": output})
 
-        print(output)
+        print(colored("<<<", "red"), output)
+        print()
