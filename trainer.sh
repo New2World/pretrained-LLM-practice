b244bfc72ae7bc1b0bc8221b00a2b2efc20d1b23
Ritchie <ruw400@psu.edu>
2025-09-06 11:09:27 -0400

    update trainer script

--- trainer.sh
+++ trainer.sh
@@ -1,13 +1,20 @@
 export TRITON_CACHE_DIR="/scratch/ruw400/.hf_hub"
 export HF_HUB_CACHE="/scratch/ruw400/.hf_hub"
 
+# 1 epoch:
+# full dataset   --> approx 62600 steps
+# training (80%) --> approx 50080 steps
+
+# to solve OOM problem
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+
 accelerate launch --num_processes=1 --num_machines=1 --mixed_precision=bf16 \
-    train_trainer.py  --max_steps=62600 \
+    train_trainer.py  --max_steps=50080 \
                       --batch_size=1 \
                       --grad_accum=16 \
                       --dtype=bf16 \
-                      --init_lr=1e-4 \
-                      --min_lr=5e-5 \
+                      --init_lr=5e-5 \
+                      --min_lr=1e-5 \
                       --grad_checkpointing \
                       --lora_r=8 \
                       --lora_alpha=16 \
